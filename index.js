1d8760915d8110e2feb36fd88a5376cfa067f1fb
hermitex <mercytibs@yahoo.com>
2022-06-22 18:35:49 +0300

    Add initial API config

---
+++ index.js
@@ -0,0 +1,6 @@
+// import dotenv from './node_modules/dotenv'
+
+// dotenv.config();
+
+const API_KEY = 'AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s'
+const Client_ID = '711203027374-ifu796qdf1tn2flaho92h9dvbukit70b.apps.googleusercontent.com'
