f6e86503fee81f2546996a02fd71da34f4d59f44
hermitex <mercytibs@yahoo.com>
2022-06-23 01:31:16 +0300

    fix API key issues

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
-const decodeJwtResponse=(credentials)=>credentials;
-function handleCredentialResponse(response) {
-  console.log(response.credential);
-  const responsePayload = decodeJwtResponse(response.credential);
-  console.log("ID: " + responsePayload.sub);
-  console.log("Full Name: " + responsePayload.name);
-  console.log("Given Name: " + responsePayload.given_name);
-  console.log("Family Name: " + responsePayload.family_name);
-  console.log("Image URL: " + responsePayload.picture);
-  console.log("Email: " + responsePayload.email);
+// const decodeJwtResponse=(credentials)=>credentials;
+// function handleCredentialResponse(response) {
+//   console.log(response.credential);
+//   const responsePayload = decodeJwtResponse(response.credential);
+//   console.log("ID: " + responsePayload.sub);
+//   console.log("Full Name: " + responsePayload.name);
+//   console.log("Given Name: " + responsePayload.given_name);
+//   console.log("Family Name: " + responsePayload.family_name);
+//   console.log("Image URL: " + responsePayload.picture);
+//   console.log("Email: " + responsePayload.email);
+// }
+
+/**
+ * Sample JavaScript code for youtube.liveBroadcasts.insert
+ * See instructions for running APIs Explorer code samples locally:
+ * https://developers.google.com/explorer-help/code-samples#javascript
+ */
+
+function authenticate() {
+  return gapi.auth2
+    .getAuthInstance()
+    .signIn({
+      scope: "https://www.googleapis.com/auth/youtube.force-ssl",
+    })
+    .then(
+      function () {
+        alert("Sign-in successful");
+      },
+      function (err) {
+        alert("Error signing in", err.message);
+      }
+    );
 }
 
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s");
+  gapi.client.setApiKey("AIzaSyDh5GjniJociXMPX2C8cghEwd0wejtQYJc");
   return gapi.client
     .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
     .then(
@@ -38,6 +60,10 @@ function execute() {
         console.error("Execute error", err);
       }
     );
-} gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "542407401873-ibaetrs6gh4cu749u7hvg2vagr238psh.apps.googleusercontent.com"});
+}
+gapi.load("client:auth2", function () {
+  gapi.auth2.init({
+    client_id:
+      "542407401873-ibaetrs6gh4cu749u7hvg2vagr238psh.apps.googleusercontent.com",
   });
+});
