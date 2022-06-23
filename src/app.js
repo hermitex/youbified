0dbd2299e38fc4dcc5045f65f09b5258fa7462cd
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-06-23 17:00:46 +0300

    facillitate fetching and display events on the frontend

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const baseUrl = "http://localhost:3001";
 
 const createNewLiveStreamBtn = document.querySelector("#createNewLiveStream");
 const viewAllLiveStreamsBtn = document.querySelector("#viewAllLiveStreams");
+const streamList = document.querySelector("#stream-list");
 
 /**
  * @createNewLiveStream
@@ -22,11 +23,35 @@ const createNewLiveStream = () => {
  * The successfully resolved promise is an object with a list of streams
  */
 const viewAllLiveStreams = () => {
+  streamList.innerHTML = "";
   fetch(`${baseUrl}/streams`)
     .then((response) => response.json())
-    .then((response) => console.log(response))
+    .then((response) => {
+      response.docs.forEach((stream) => {
+        streamList.innerHTML += `
+        <tr>
+        <td> <input type="checkbox" class="cotrol active" name="stream" value="${
+          stream.id
+        }" /></td>
+        <td>${Date(stream.creation_time).split(" ").splice(1, 3).join(" ")}</td>
+        <td><input type="text" value="${stream.playback.embed_url}"></td>       
+        <td>${stream.broadcasting_status}</td>
+       
+        <td>
+        <button type="button" class="btn btn-info" onclick="attendStream(streamList.checked)">Attend</button>
+        </td>
+        <td>
+        <button type="button" class="btn btn-danger" onclick="deleteStream(streamList.checked)">Delete</button>
+        </td>
+       
+        </tr>
+        `;
+      });
+    })
     .catch((err) => console.error(err));
 };
+
+viewAllLiveStreams();
 
 const getLiveStremById = (id) => {
   fetch(`${baseUrl}/streams/${id}`)
