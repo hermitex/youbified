0b9e887618614dca381f881779518e00f70afe73
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-06-23 10:49:00 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -13,19 +13,33 @@ const decodeJwtResponse = (credentials) => {
 
   return JSON.parse(jsonPayload);
 };
-let responsePayload;
-function handleCredentialResponse(response) {
-  responsePayload = decodeJwtResponse(response.credential);
-  console.log("ID: " + responsePayload.sub);
-  console.log("Full Name: " + responsePayload.name);
-  console.log("Given Name: " + responsePayload.given_name);
-  console.log("Family Name: " + responsePayload.family_name);
-  console.log("Image URL: " + responsePayload.picture);
-  console.log("Email: " + responsePayload.email);
+
+// function handleCredentialResponse(response) {
+//   const responsePayload = decodeJwtResponse(response.credential);
+//   console.log("ID: " + responsePayload.sub);
+//   console.log("Full Name: " + responsePayload.name);
+//   console.log("Given Name: " + responsePayload.given_name);
+//   console.log("Family Name: " + responsePayload.family_name);
+//   console.log("Image URL: " + responsePayload.picture);
+//   console.log("Email: " + responsePayload.email);
+// }
+
+function authenticate() {
+  return gapi.auth2
+    .getAuthInstance()
+    .signIn({ scope: "https://www.googleapis.com/auth/youtube.force-ssl" })
+    .then(
+      function () {
+        console.log("Sign-in successful");
+      },
+      function (err) {
+        console.error("Error signing in", err);
+      }
+    );
 }
 
 function loadClient() {
-  gapi.client.setApiKey("AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s");
+  gapi.client.setApiKey("AIzaSyDh5GjniJociXMPX2C8cghEwd0wejtQYJc");
   return gapi.client
     .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
     .then(
@@ -39,13 +53,24 @@ function loadClient() {
 }
 
 function execute() {
-  gapi.client.load("plus", "v1", function () {
-    var request = gapi.client.plus.people.get({
-      userId: "AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s",
-    });
-
-    request.execute(function (response) {
-      console.log("Here ", response);
-    });
-  });
+  return gapi.client.youtube.liveBroadcasts
+    .insert({
+      resource: {},
+    })
+    .then(
+      function (response) {
+        // Handle the results here (response.result has the parsed body).
+        console.log("Response", response);
+      },
+      function (err) {
+        console.error("Execute error", err.body);
+      }
+    );
 }
+
+gapi.load("client:auth2", function () {
+  gapi.auth2.init({
+    client_id:
+      "542407401873-ibaetrs6gh4cu749u7hvg2vagr238psh.apps.googleusercontent.com",
+  });
+});
