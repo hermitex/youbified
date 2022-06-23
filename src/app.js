28b0f2354621bb5da1392888c59dab5f5a02d3ac
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-06-23 17:26:05 +0300

    chnage base url

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-const baseUrl = "http://localhost:3001";
+// const baseUrl = "http://localhost:3001";
+const baseUrl = "https://youbified.herokuapp.com";
 
 const createNewLiveStreamBtn = document.querySelector("#createNewLiveStream");
 const viewAllLiveStreamsBtn = document.querySelector("#viewAllLiveStreams");
