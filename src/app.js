3f1fa733cd7aa0bed9ac880f35182c6089ed7efb
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-06-23 09:57:31 +0300

    fix authentication

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
-const decodeJwtResponse=(credentials)=>{
-  var base64Url = credentials.split('.')[1];
-  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-  var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-  }).join(''));
+const decodeJwtResponse = (credentials) => {
+  var base64Url = credentials.split(".")[1];
+  var base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  var jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split("")
+      .map(function (c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
 
   return JSON.parse(jsonPayload);
 };
-let  responsePayload;
+let responsePayload;
 function handleCredentialResponse(response) {
   responsePayload = decodeJwtResponse(response.credential);
   console.log("ID: " + responsePayload.sub);
@@ -18,16 +24,28 @@ function handleCredentialResponse(response) {
   console.log("Email: " + responsePayload.email);
 }
 
+function loadClient() {
+  gapi.client.setApiKey("AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s");
+  return gapi.client
+    .load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+    .then(
+      function () {
+        console.log("GAPI client loaded for API");
+      },
+      function (err) {
+        console.error("Error loading GAPI client for API", err);
+      }
+    );
+}
+
 function execute() {
-  
-  gapi.client.load('plus', 'v1', function() {
-
+  gapi.client.load("plus", "v1", function () {
     var request = gapi.client.plus.people.get({
-        'userId': "AIzaSyDh5GjniJociXMPX2C8cghEwd0wejtQYJc"
+      userId: "AIzaSyC4sIqO6MbIkFTzWdjDr-4ulv4DpVydH7s",
     });
 
-    request.execute(function(response) {
-        console.log(response);   
+    request.execute(function (response) {
+      console.log("Here ", response);
     });
-});
+  });
 }
